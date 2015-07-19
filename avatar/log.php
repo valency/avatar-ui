d3e356cfa4d3c44763afac26743f2a7bdc01521a
Ye Ding <guiewy@gmail.com>
2015-07-19 19:41:43 +0800

    v0.7.19

--- avatar/log.php
+++ avatar/log.php
@@ -6,9 +6,17 @@
 <body>
 <h1>Project Avatar</h1>
 
-<h3>Last Update: <?php echo date("F d Y, H:i:s", filemtime("./index.php")); ?></h3>
+<p>Last Update: <?php echo date("F d Y, H:i:s", filemtime("./index.php")); ?></p>
+<hr/>
 <ul>
     <li><a href="upload.php">CSV Data Uploader</a> (Last Update: <?php echo date("F d Y, H:i:s", filemtime("./upload.php")); ?>)</li>
+</ul>
+<hr/>
+<h2>Updates</h2>
+<hr/>
+<h3>2015-07-19:</h3>
+<ul>
+    <li>Added import map from local file</li>
 </ul>
 </body>
 </html>
