294e5df776f887eea66ee85b2492fce0f3bf82fd
ArMPY <hcwenyc@gmail.com>
2021-02-23 14:16:02 +0800

    .

--- js/conf.js
+++ js/conf.js
@@ -1,4 +1,4 @@
-var API_SERVER = "http://" + location.hostname + "/api/";
+var API_SERVER = "http://map.numb.im/";
 var COLOR_PALETTE = ["#467D97", "#5DA5DA", "#FAA43A", "#60BD68", "#F17CB0", "#B2912F", "#B276B2", "#DECF3F", "#F15854", "#A03423"];
 var LAT_OFFSET = 0.0060;
 var LNG_OFFSET = 0.0065;
