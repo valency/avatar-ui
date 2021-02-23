2ee198912b33fc334c5281c63359580b821e6843
Yuchen <hcwenyc@gmail.com>
2021-02-23 15:30:53 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -132,115 +132,115 @@ function plot() {
         points.push(p.p);
         p["marker"] = L.Marker({
             latlng: p.p,
-            title: i.toString(),
-            draggable: traj.path != null
+            title: i.toString()
         }).addTo(map);
+        console.log(p["marker"]);
         // Handel marker events
-        p["marker"].addListener("click", function () {
-            var sequence = this.title;
-            var msg = "<span class='bold text-danger'>Point " + sequence + "</span><hr/>";
-            msg += "<span class='bold'>ID: </span>" + traj.trace.p[sequence]["id"] + "<br/>";
-            msg += "<span class='bold'>Time Stamp: </span>" + traj.trace.p[sequence]["t"] + "<br/>";
-            msg += "<span class='bold'>Latitude : </span>" + traj.trace.p[sequence]["p"]["lat"] + "<br/>";
-            msg += "<span class='bold'>Longitude: </span>" + traj.trace.p[sequence]["p"]["lng"] + "<br/>";
-            msg += "<span class='bold'>Speed: </span>" + traj.trace.p[sequence]["speed"] + "<br/>";
-            msg += "<span class='bold'>Angle: </span>" + traj.trace.p[sequence]["angle"] + "<br/>";
-            msg += "<span class='bold'>Occupancy: </span>" + traj.trace.p[sequence]["occupy"];
-            if (traj.path) {
-                var path_road_sequence = find_map_matched_road_from_path(sequence);
-                msg += "<br/><span class='bold'>Map-Matched Road: </span>" + traj.path.road[path_road_sequence].road.id;
-                render_road(traj.path.road[path_road_sequence].road.id, MAP_MATCHED_ROAD_COLOR, function (road_object) {
-                    map_matched_road = road_object;
-                });
-            }
-            if (info_window) {
+//         p["marker"].addListener("click", function () {
+//             var sequence = this.title;
+//             var msg = "<span class='bold text-danger'>Point " + sequence + "</span><hr/>";
+//             msg += "<span class='bold'>ID: </span>" + traj.trace.p[sequence]["id"] + "<br/>";
+//             msg += "<span class='bold'>Time Stamp: </span>" + traj.trace.p[sequence]["t"] + "<br/>";
+//             msg += "<span class='bold'>Latitude : </span>" + traj.trace.p[sequence]["p"]["lat"] + "<br/>";
+//             msg += "<span class='bold'>Longitude: </span>" + traj.trace.p[sequence]["p"]["lng"] + "<br/>";
+//             msg += "<span class='bold'>Speed: </span>" + traj.trace.p[sequence]["speed"] + "<br/>";
+//             msg += "<span class='bold'>Angle: </span>" + traj.trace.p[sequence]["angle"] + "<br/>";
+//             msg += "<span class='bold'>Occupancy: </span>" + traj.trace.p[sequence]["occupy"];
+//             if (traj.path) {
+//                 var path_road_sequence = find_map_matched_road_from_path(sequence);
+//                 msg += "<br/><span class='bold'>Map-Matched Road: </span>" + traj.path.road[path_road_sequence].road.id;
+//                 render_road(traj.path.road[path_road_sequence].road.id, MAP_MATCHED_ROAD_COLOR, function (road_object) {
+//                     map_matched_road = road_object;
+//                 });
+//             }
+//             if (info_window) {
                 
-                if (map_matched_road) map_matched_road.setMap(null);
-                map_matched_road = null;
-            }
-            info_window = p["marker"].bindPopup(msg);
-            info_window.addListener("closeclick", function () {
-                if (map_matched_road) map_matched_road.setMap(null);
-                map_matched_road = null;
-            });
-            info_window.openPopup(map);
-        });
-        if (traj.path) {
-            p["marker"].addListener("dragstart", function (type, target) {
-                if (info_window) {
+//                 if (map_matched_road) map_matched_road.setMap(null);
+//                 map_matched_road = null;
+//             }
+//             info_window = p["marker"].bindPopup(msg);
+//             info_window.addListener("closeclick", function () {
+//                 if (map_matched_road) map_matched_road.setMap(null);
+//                 map_matched_road = null;
+//             });
+//             info_window.openPopup(map);
+//         });
+//         if (traj.path) {
+//             p["marker"].addListener("dragstart", function (type, target) {
+//                 if (info_window) {
                     
-                    if (map_matched_road) map_matched_road.setMap(null);
-                    map_matched_road = null;
-                }
-                var sequence = this.title;
-                // Draw current road
-                render_road(traj.path.road[find_map_matched_road_from_path(sequence)].road.id, MAP_MATCHED_ROAD_COLOR, function (road_object) {
-                    map_matched_road = road_object;
-                });
-                // Draw candidate roads
-                candidate_roads = [];
-                $.get(API_SERVER + "avatar/map-matching/find_candidates/?city=" + $("#search-city").val() + "&lat=" + traj.trace.p[sequence]["p"]["lat"] + "&lng=" + traj.trace.p[sequence]["p"]["lng"], function (candidates) {
-                    for (var i = 0; i < Math.min(10, candidates.length); i++) {
-                        render_road(candidates[i], CANDIDATE_ROAD_COLOR, function (road_object) {
-                            candidate_roads.push(road_object);
-                            // Only show road if dragging is still on
-                            if (!dragging) road_object.setMap(null);
-                        });
-                    }
-                });
-                // Remember current position
-                drag_marker_position = this.getPosition();
-                // Init target road object
-                drag_road = {rendered: true};
-                dragging = true;
-            });
-            p["marker"].addListener("drag", function (type, target, pixel, point) {
-                if (drag_road.rendered) {
-                    drag_road.rendered = false;
-                    $.get(API_SERVER + "avatar/map-matching/find_candidates/?city=" + $("#search-city").val() + "&lat=" + this.getPosition().lat() + "&lng=" + this.getPosition().lng(), function (candidates) {
-                        // Clear previous render
-                        if (drag_road.object) drag_road.object.setMap(null);
-                        // Render
-                        drag_road.id = candidates[0];
-                        render_road(drag_road.id, TARGET_ROAD_COLOR, function (road_object) {
-                            drag_road.object = road_object;
-                            drag_road.rendered = true;
-                            // Only show road if dragging is still on
-                            if (!dragging) road_object.setMap(null);
-                        });
-                    });
-                }
-            });
-            p["marker"].addListener("dragend", function (type, target, pixel, point) {
-                var sequence = this.title;
-                var p = traj["trace"]["p"][sequence];
-                // Stop dragging flag
-                dragging = false;
-                // Clean up previous road
-                if (map_matched_road) map_matched_road.setMap(null);
-                // Clean up target road
-                if (drag_road.object) drag_road.object.setMap(null);
-                // Clean up all candidate roads
-                for (var i = 0; i < candidate_roads.length; i++) {
-                    if (candidate_roads[i]) candidate_roads[i].setMap(null);
-                }
-                // Push back to original position
-                this.setPosition(drag_marker_position);
-                // Re-perform map-matching
-                console.log("Trying to re-perform map-matching via: id = " + traj["id"] + ", pid = " + p["id"] + ", rid = " + drag_road["id"] + ", uid = " + $.cookie('avatar_id'));
-                bootbox.dialog({
-                    message: "<i class='fa fa-spinner'></i> Performing map-matching, please be patient...",
-                    closeButton: false
-                });
-                $.get(API_SERVER + "avatar/map-matching/perform_with_label/?city=" + $("#search-city").val() + "&id=" + traj["id"] + "&pid=" + p["id"] + "&rid=" + drag_road["id"] + "&uid=" + $.cookie('avatar_id'), function (data) {
-                    bootbox.hideAll();
-                    search(traj["id"]);
-                }).fail(function () {
-                    bootbox.hideAll();
-                    bootbox.alert("<span class='text-danger'><i class='fa fa-exclamation-triangle'></i> Something is wrong during map-matching!</span>");
-                });
-            });
-        }
+//                     if (map_matched_road) map_matched_road.setMap(null);
+//                     map_matched_road = null;
+//                 }
+//                 var sequence = this.title;
+//                 // Draw current road
+//                 render_road(traj.path.road[find_map_matched_road_from_path(sequence)].road.id, MAP_MATCHED_ROAD_COLOR, function (road_object) {
+//                     map_matched_road = road_object;
+//                 });
+//                 // Draw candidate roads
+//                 candidate_roads = [];
+//                 $.get(API_SERVER + "avatar/map-matching/find_candidates/?city=" + $("#search-city").val() + "&lat=" + traj.trace.p[sequence]["p"]["lat"] + "&lng=" + traj.trace.p[sequence]["p"]["lng"], function (candidates) {
+//                     for (var i = 0; i < Math.min(10, candidates.length); i++) {
+//                         render_road(candidates[i], CANDIDATE_ROAD_COLOR, function (road_object) {
+//                             candidate_roads.push(road_object);
+//                             // Only show road if dragging is still on
+//                             if (!dragging) road_object.setMap(null);
+//                         });
+//                     }
+//                 });
+//                 // Remember current position
+//                 drag_marker_position = this.getPosition();
+//                 // Init target road object
+//                 drag_road = {rendered: true};
+//                 dragging = true;
+//             });
+//             p["marker"].addListener("drag", function (type, target, pixel, point) {
+//                 if (drag_road.rendered) {
+//                     drag_road.rendered = false;
+//                     $.get(API_SERVER + "avatar/map-matching/find_candidates/?city=" + $("#search-city").val() + "&lat=" + this.getPosition().lat() + "&lng=" + this.getPosition().lng(), function (candidates) {
+//                         // Clear previous render
+//                         if (drag_road.object) drag_road.object.setMap(null);
+//                         // Render
+//                         drag_road.id = candidates[0];
+//                         render_road(drag_road.id, TARGET_ROAD_COLOR, function (road_object) {
+//                             drag_road.object = road_object;
+//                             drag_road.rendered = true;
+//                             // Only show road if dragging is still on
+//                             if (!dragging) road_object.setMap(null);
+//                         });
+//                     });
+//                 }
+//             });
+//             p["marker"].addListener("dragend", function (type, target, pixel, point) {
+//                 var sequence = this.title;
+//                 var p = traj["trace"]["p"][sequence];
+//                 // Stop dragging flag
+//                 dragging = false;
+//                 // Clean up previous road
+//                 if (map_matched_road) map_matched_road.setMap(null);
+//                 // Clean up target road
+//                 if (drag_road.object) drag_road.object.setMap(null);
+//                 // Clean up all candidate roads
+//                 for (var i = 0; i < candidate_roads.length; i++) {
+//                     if (candidate_roads[i]) candidate_roads[i].setMap(null);
+//                 }
+//                 // Push back to original position
+//                 this.setPosition(drag_marker_position);
+//                 // Re-perform map-matching
+//                 console.log("Trying to re-perform map-matching via: id = " + traj["id"] + ", pid = " + p["id"] + ", rid = " + drag_road["id"] + ", uid = " + $.cookie('avatar_id'));
+//                 bootbox.dialog({
+//                     message: "<i class='fa fa-spinner'></i> Performing map-matching, please be patient...",
+//                     closeButton: false
+//                 });
+//                 $.get(API_SERVER + "avatar/map-matching/perform_with_label/?city=" + $("#search-city").val() + "&id=" + traj["id"] + "&pid=" + p["id"] + "&rid=" + drag_road["id"] + "&uid=" + $.cookie('avatar_id'), function (data) {
+//                     bootbox.hideAll();
+//                     search(traj["id"]);
+//                 }).fail(function () {
+//                     bootbox.hideAll();
+//                     bootbox.alert("<span class='text-danger'><i class='fa fa-exclamation-triangle'></i> Something is wrong during map-matching!</span>");
+//                 });
+//             });
+//        }
     }
     console.log(points);
     var polyline = L.polyline({latlng: points[0]}, {color: TRACE_COLOR}).addTo(map);
